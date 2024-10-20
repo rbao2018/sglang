64452062ad4f7d4f51f3c02b872dd4950d924074
rbao2018 <rbao2018@outlook.com>
2024-10-20 15:03:17 +0800

    reverse reward model code

--- python/sglang/srt/models/llama_reward.py
+++ python/sglang/srt/models/llama_reward.py
@@ -62,7 +62,7 @@ class LlamaForSequenceClassification(nn.Module):
         params_dict = dict(self.named_parameters())
 
         for name, loaded_weight in weights:
-            if "score" in name:
+            if "classification_head" in name:
                 param = params_dict[name]
                 weight_loader = getattr(param, "weight_loader", default_weight_loader)
                 weight_loader(param, loaded_weight)
@@ -125,7 +125,7 @@ class LlamaForSequenceClassificationWithNormal_Weights(LlamaForSequenceClassific
         params_dict = dict(self.named_parameters())
 
         for name, loaded_weight in weights:
-            if "score" in name:
+            if "classification_head" in name:
                 param = params_dict[name]
                 weight_loader = getattr(param, "weight_loader", default_weight_loader)
                 weight_loader(param, loaded_weight)
